60e8fc8f88e584aa6c72cad29ad9cfc04e691b6a
bharat saini <sainibharat277@gmail.com>
2024-03-22 22:10:40 +0530

    first commit

---
+++ server/controllers/SignUp.js
@@ -0,0 +1,59 @@
+import dotenv from "dotenv";
+dotenv.config({
+  path: "../.env",
+});
+import User from "../Models/User.js";
+import createErrors from "../Errors/createErrors.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const SignUp = async (req, res, next) => {
+  try {
+    let { firstName, lastName, email, phone, password, confirmPassword } =
+      req.body;
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !phone ||
+      !password ||
+      !confirmPassword
+    )
+      return res.json(createErrors(404, "all fields are required", false));
+      const validEmail=validator.isEmail(email);
+      const validPhone=validator.isMobilePhone(phone);
+      if(!validEmail||!validPhone) return res.json(createErrors(400,"email or phone incorrect",false));
+    const checkEmail = await User.findOne({
+      email: req.body.email,
+    });
+    if (checkEmail)
+      return res.json(createErrors(405, "user already exist", false));
+    if (req.body.password !== req.body.confirmPassword)
+      return res.json(
+        createErrors(400, "password and confirm password different", false)
+      );
+    password = await bcrypt.hash(req.body.password, 10);
+
+    const user = await new User({
+      firstName,
+      lastName,
+      email,
+      phone,
+      password,
+    });
+    await user.save();
+    const token = await jwt.sign(
+      { _id: user._id },
+      process.env.SECRETE_TOKEN_KEY
+    );
+    return res.status(200).json({
+      success: true,
+      message: "user created successfully",
+      token,
+      user,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+export default SignUp;
